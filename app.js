8b6fe307ff016a86b3fbc0e8fb51ff9eb8acca92
pancho <fj.novoap@gmail.com>
2021-11-23 14:44:48 -0300

    fixing cors try 3

--- app.js
+++ app.js
@@ -9,11 +9,21 @@ const { requestLogger } = require("./utils/requestlogger");
 const { errorHandler } = require("./utils/errorHandler");
 const { unknownEndpoint } = require("./utils/unknownEndPoint");
 
-app.use(
-  cors({
-    origin: "https://elegant-stonebraker-898f21.netlify.app",
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 
